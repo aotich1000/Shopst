7ba885e89c52a7cc004d00df81e5109604c78cbf
vi2k4 <vhuynh414@gmail.com>
2023-11-21 16:10:52 +0700

    dung loigit add .

--- admin_final.js
+++ admin_final.js
@@ -34,7 +34,7 @@ function loadTable(index){
                     </tr>
                 </thead><tbody id="itemTableBody">`;
     for(var i=0; i<index.length;i++){
-        s+=` 
+        s+=`    
         <tr>
             <th>${index[i].id}</th>
             <th><img src="${index[i].img}" alt="a"></th>
@@ -52,7 +52,6 @@ function loadTable(index){
 
 
 function exitWebsite() {
-    // Chuyển hướng người dùng đến một trang khác hoặc trang chính
     window.location.href = 'https://www.youtube.com/';
 }
 
@@ -67,50 +66,56 @@ function exitWebsite() {
 window.onload = function(){
     existingProducts = JSON.parse(localStorage.getItem('product')) || [];
 
-
     var addProductForm = document.getElementById('addProductForm');
     addProductForm.addEventListener('submit', function(event) {
         if (event.submitter && event.submitter.id === 'add-submit') {
             
-            event.preventDefault(); // Ngăn chặn form tự động gửi dữ liệu và làm tải lại trang
+            event.preventDefault(); 
 
             var productId = document.getElementById('productId').value;
             var productName = document.getElementById('productName').value;
-            var productImg = document.getElementById('productImg').files[0]; // Lấy file hình ảnh
+            var productImg = document.getElementById('productImg').files[0]; 
             var productCost = document.getElementById('productCost').value;
+            productImg.name //Value filename 
 
-            // Kiểm tra xem có hình ảnh được chọn không
-            if (productImg) {
-                var reader = new FileReader();
-                reader.onload = function(e) {
-                    var newProduct = {
-                        id: productId,
-                        name: productName,
-                        img: e.target.result, // Đường dẫn hình ảnh
-                        cost: productCost + "$",
-                    };
+            // var reader = new FileReader();
+            // reader.onload = function(e) {
+                var newProduct = {
+                    id: productId,
+                    name: productName,
+                    img: "img/"+productImg.name,  //e.target.result, // Đường dẫn hình ảnh
+                    cost: productCost + "$",
+                };
+                // if (isProductIdExists(newProduct.id)) {
                     existingProducts.push(newProduct);
                     localStorage.setItem('product', JSON.stringify(existingProducts));
-                    renderPageNumber(); 
+                    renderPageNumber();
 
                     clearValueProduct();
                     alert('Sản phẩm đã được thêm thành công!');
-                };
-                reader.readAsDataURL(productImg); // Đọc hình ảnh dưới dạng URL data
-            } else {
-                alert('Vui lòng chọn hình ảnh sản phẩm!');
-            }
+                // } else {
+                //     alert('ID sản phẩm đã tồn tại. Vui lòng chọn một ID khác.');
+                // }
+            // };
+            // reader.readAsDataURL(productImg);
         }
        
     });
 
+    // function isProductIdExists(productId) {
+    //     existingProducts = JSON.parse(localStorage.getItem('product')) || [];
+    //     for(let i = 0 ; i < existingProducts.length ; i++){
+    //         if(existingProducts[i] == productId){
+    //             return false;
+    //         }
+    //     }
+    // }
     
 
 
     
 // -------------------Xóa sản phẩm--------------------
 
-    // Update the deleteProduct function
     function deleteProduct(productId) {
         var confirmation = confirm('Bạn có chắc chắn muốn xóa sản phẩm này không?');
     
@@ -123,7 +128,6 @@ window.onload = function(){
                 existingProducts.splice(index, 1);
                 localStorage.setItem('product', JSON.stringify(existingProducts));
                 renderPageNumber();
-                
                 alert('Hãy nhấn vào Số trang bên dưới để load lại danh sách sản phẩm!');
             } else {
                 alert('Không tìm thấy sản phẩm để xóa!');
@@ -270,48 +274,58 @@ document.getElementById('productList').addEventListener('click', function (event
     var target = event.target;
 
     if (target.tagName === 'BUTTON' && target.textContent.trim() === 'Sửa') {
-        var productId = target.parentElement.parentElement.querySelector('th:first-child').textContent;
+        var productId = target.closest('tr').querySelector('th:first-child').textContent;
         editProduct(productId);
     }
 });
 
 function editProduct(productId) {
-    var existingProducts = JSON.parse(localStorage.getItem('product')) || [];
-    document.getElementById('index').value = productId - 1;
-    document.getElementById('productId').value = existingProducts[productId-1].id;
-    document.getElementById('productName').value = existingProducts[productId-1].name;
-    document.getElementById('productCost').value = existingProducts[productId-1].cost.replace('$', '');
-    document.getElementById('addProduct').style.display = "block";
-    document.getElementById('h2-add').style.display = "none"
-    document.getElementById('h2-cha').style.display = "block"
-    document.getElementById('add-submit').style.display = "none"
-    document.getElementById('add-update').style.display = "block"
-}
 
+    var existingProducts = JSON.parse(localStorage.getItem('product')) || [];
+
+    var index = existingProducts.findIndex(function (product) {
+        return product.id == productId;
+    });
+
+    if (index !== -1) {
+        document.getElementById('index').value = index;
+        document.getElementById('productId').value = existingProducts[index].id;
+        document.getElementById('productName').value = existingProducts[index].name;
+        document.getElementById('productCost').value = existingProducts[index].cost.replace('$', '');
+        document.getElementById('addProduct').style.display = "block";
+        document.getElementById('h2-add').style.display = "none"
+        document.getElementById('h2-cha').style.display = "block"
+        document.getElementById('add-submit').style.display = "none"
+        document.getElementById('add-update').style.display = "block"
+    }
+}
 
 function changeProduct(){
 
     var existingProducts = JSON.parse(localStorage.getItem('product')) || [];
     var index = document.getElementById('index').value;
     var tempProduct =[];
-    var productImg = document.getElementById('productImg').files[0];
-    var reader = new FileReader();
-    reader.onload = function(e){
+    var productImg = document.getElementById('productImg').files[0];    
+    // var reader = new FileReader();
+    // // console.log('a');
+    // reader.onload = function(e){
         tempProduct = {
             id : document.getElementById('productId').value,
-            name : document.getElementById('productName').value,
-            img : e.target.result,
+            name : productImg.value,
+            img : "img/" +  document.getElementById('productImg').name,
             cost : document.getElementById('productCost').value + "$"
         }
+        console.log(tempProduct);
         existingProducts[index] = tempProduct;
         localStorage.setItem("product", JSON.stringify(existingProducts));
         renderPageNumber();
         document.getElementById('addProduct').style.display = "none";
         clearValueProduct();
     }
-    reader.readAsDataURL(productImg);
-
-}
+    // reader.readAsDataURL(productImg);
+    
+//     reader.readAsDataURL()
+// }
 
 
 
